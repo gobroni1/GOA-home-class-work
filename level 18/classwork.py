c235bb03637b74c023c5a9aa0f0202e5d1d0a9d1
gobroni tsitlauri <deathshadow075@gmail.com>
2024-06-20 06:45:00 -0700

    no comment

--- level 18/classwork.py
+++ level 18/classwork.py
@@ -1,4 +1,12 @@
 list1 = []
-for i in range (11):
+for i in range (10):
     list1.append(i)
+print(len(list1))
+
+num1 = int(input("num1 pls "))
+num2 = int(input("num2 pls "))
+
+for i in range (num1, num2):
+    list1.append(i)
+print(list1)
 print(len(list1))
