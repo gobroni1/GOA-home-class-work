7036b1d703ca7edca64a75ed42d233ca26706683
gobroni tsitlauri <deathshadow075@gmail.com>
2024-06-20 06:53:40 -0700

    bo

--- level 18/classwork.py
+++ level 18/classwork.py
@@ -11,3 +11,14 @@ for i in range (num1, num2):
 print(list1)
 print(len(list1))
 
+
+list_1 = []
+starting_n = int(input("starting num pls:"))
+ending_n = int(input("ending num pls:"))
+
+for i in range (starting_n, ending_n):
+    list_1.append(i)
+print(list_1)
+print(max(list_1))
+print(min(list_1))
+print(sum(list_1))
