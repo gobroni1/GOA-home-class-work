d0ae62cec3405378fb30231b12ca4bfa9b7266a5
gobroni tsitlauri <deathshadow075@gmail.com>
2024-07-02 02:51:22 -0700

    commit

--- level 23/classwork_23.py
+++ level 23/classwork_23.py
@@ -3,3 +3,13 @@ def ma (s,intlist1):
         s += i
     return s / len(intlist1)
 print(ma(0,[2,5,7]))
+
+#
+#
+
+def d_or_m (a,b,c):
+    if a == "/":
+        return b / c
+    else:
+        return b * c
+print(d_or_m(input("what to do? '/' or '*' "),4,2))
