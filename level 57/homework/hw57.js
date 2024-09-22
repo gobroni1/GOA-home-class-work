d0a7233535eb959ac7716102e2a9f2affd1047f8
gobroni tsitlauri <deathshadow075@gmail.com>
2024-09-22 19:42:04 +0400

    day 58

--- level 57/homework/hw57.js
+++ level 57/homework/hw57.js
@@ -1,15 +1,33 @@
-// const massage = "hello world";
-// console.log(massage);
+// section one
+const massage = "hello world";
+console.log(massage);
 
-// let counter = 0;
-// console.log(counter);
+let counter = 0;
+console.log(counter);
 
-// counter = 10;
-// console.log(counter);
+counter = 10;
+console.log(counter);
 
-// const by = "7 november 2008";
-// console.log(by);
+const by = "7 november 2008";
+console.log(by);
 
 let x = 27;
 let y = 59;
 console.log(x+y);
+
+const adult = True;
+console.log(adult);
+
+// section two
+let a = 27;
+let b = 59;
+console.log(a+b);
+
+let c = 27;
+let d = 59;
+console.log(a*b);
+
+// section three
+let num1 = 39;
+let num2 = 78;
+console.log (num1 > num2)
---
+++ level 58/classwork/main.js
@@ -0,0 +1,11 @@
+const aladdin = {
+    name: "aladdin",
+    surname: "I don't know",
+    age: "may be 17-18"
+};
+
+console.log(aladdin.surname); 
+
+delete aladdin.surname;
+
+console.log(aladdin);
